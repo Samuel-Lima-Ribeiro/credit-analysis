bd5f8dc15529b7aec775dbfff12c8e13bc3c1d0a
Samuel-Lima-Ribeiro <samuel.ribeiro@sptech.school>
2023-05-16 11:24:05 -0300

    reajustes no swagger, tabela e aplication.yml

--- data/table.sql
+++ data/table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS ANALYSIS(
     approved boolean NOT NULL,
     approvedLimit decimal(10,2) NOT NULL,
     requestedAmount decimal(10, 2) NOT NULL,
-    withdraw boolean NOT NULL,
+    withdraw decimal(10, 2) NOT NULL,
     annualInterest decimal(10, 2) NOT NULL,
     clientId uuid NOT NULL,
     date timestamp,
