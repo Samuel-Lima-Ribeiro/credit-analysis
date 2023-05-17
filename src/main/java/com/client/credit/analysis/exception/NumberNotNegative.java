f8efa021b81333afd04a5b91648547e208d3eb08
Samuel-Lima-Ribeiro <samuel.ribeiro@sptech.school>
2023-05-17 15:16:02 -0300

    adicionaro controllerHandler e exception de contexto

---
+++ src/main/java/com/client/credit/analysis/exception/NumberNotNegative.java
@@ -0,0 +1,7 @@
+package com.client.credit.analysis.exception;
+
+public class NumberNotNegative extends RuntimeException {
+    public NumberNotNegative(String message) {
+        super(message);
+    }
+}
