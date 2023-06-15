e24b5fcba3425ba24be0002dbd7c491d7e5b4fd5
Samuel-Lima-Ribeiro <samuel.ribeiro@sptech.school>
2023-06-15 15:15:44 -0300

    adicionando grafana

--- src/main/java/com/client/credit/analysis/Application.java
+++ src/main/java/com/client/credit/analysis/Application.java
@@ -1,10 +1,14 @@
 package com.client.credit.analysis;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -16,4 +20,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
