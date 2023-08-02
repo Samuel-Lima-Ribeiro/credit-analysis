e13b37ad7f42f4931ed040c18cd34827d903adb9
Samuel-Lima-Ribeiro <110927029+Samuel-Lima-Ribeiro@users.noreply.github.com>
2023-08-02 14:58:19 -0300

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "credit-analysis-openapi-v.1.0.1.yml",
+    url: "credit-analysis-openapi-v.1.0.1.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
